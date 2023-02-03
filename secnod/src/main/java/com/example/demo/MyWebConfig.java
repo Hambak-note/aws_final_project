80770f6a005857bc104997538b1221950b30bbf6
Hambak-note <dudghks5722@gmail.com>
2023-02-03 13:52:33 +0900

    fix: MyWebConfig.java 파일, upload 경로 ec2 서버 업로드 경로로 변경

--- secnod/src/main/java/com/example/demo/MyWebConfig.java
+++ secnod/src/main/java/com/example/demo/MyWebConfig.java
@@ -7,8 +7,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MyWebConfig implements WebMvcConfigurer {
 	
+	//for linux
+	static final public String savepath = "/usr/upload/";
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/upload/**").addResourceLocations("file:///c:/upload/");
+		//for linux
+		//putty로 서버 접속 후
+		//mkdir /usr/upload 실행 후
+		registry.addResourceHandler("/upload/**")
+				.addResourceLocations("file:/usr/upload/");
+		
 	}
 }
